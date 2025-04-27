5c639ce24df0922ba30d19395879b7af42501f55
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2025-04-27 11:18:23 -0300

    Armazemamento de projeots

--- index.php
+++ index.php
@@ -13,8 +13,47 @@
     $titulo = $saudacao . ' Portfolio do ' . $nome;
     $subtitulo = 'Seja bem vindo ao meu Portfolio!!';
     $ano = 2024;
+
+    $projetos = [
+        [
+            "titulo"=>"Meu Portifolio",
+            "finalizado"=>false,
+            "data"=>"2025-03-09",
+            "descricao"=>"Meu primeiro portifolio. Escrito em PHP e HTML",
+            "linguagem" => "PHP",
+        ],
+        [
+            "titulo"=>"Estudos de Api RestFull Crud python",
+            "finalizado"=>True,
+            "data"=>"2025-01-09",
+            "descricao"=>"Meu primeiro estudo com Api Rest",
+            "linguagem"=> "Python",
+        ],
+        [
+            "titulo"=>"Estudos de Api RestFull intermediario Login e Logout",
+            "finalizado"=>True,
+            "data"=>"2025-01-20",
+            "descricao"=>"Meu Segundo estudo com Api Rest, Login e Logout, banco de dados e Codificação",
+            "linguagem"=> "Python",
+        ],
+        [
+            "titulo"=>"Projeto Player",
+            "finalizado"=>True,
+            "data"=>"2024-12",
+            "descricao"=>"Minha prova de POO utilizando PHP oritado a objetos",
+            "linguagem"=> "PHP",
+        ],
+        [
+            "titulo"=>"Projeto Escritorio",
+            "finalizado"=>True,
+            "data"=>"2024-12",
+            "descricao"=>"Umas das minhas de POO utilizando PHP oritado a objetos",
+            "linguagem"=> "PHP",
+        ],
+    ];
+
 ?>
-    
+
 <h1><?= $titulo ?></h1>
 <p><?= $subtitulo ?></p>
 <p><?php echo $ano ?></p>
